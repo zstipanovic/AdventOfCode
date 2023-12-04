d3a8d4bda545cd89571ae816f6fcdbe75aa04448
zstipanovic <zvonimir.stipanovic1@gmail.com>
2023-12-04 22:27:46 +0100

    fix part 2 day 01

--- 2023/day01/code.ts
+++ 2023/day01/code.ts
@@ -2,32 +2,37 @@ import * as fs from "fs";
 
 const input = fs.readFileSync("2023/day01/input.txt", "utf8");
 
-const numberRegex = /\d/g;
-const wordRegex = /(one|two|three|four|five|six|seven|eight|nine)/g;
+const wordRegex = /(one|two|three|four|five|six|seven|eight|nine|\d)/g;
 
 const lines = input.split("\n");
 
 let arrayOfDigits: number[] = [];
 
-for (const line of lines) {
-  const replacedNumbers = line.replace(wordRegex, (match) =>
-    match
-      .replace("one", "1")
-      .replace("two", "2")
-      .replace("three", "3")
-      .replace("four", "4")
-      .replace("five", "5")
-      .replace("six", "6")
-      .replace("seven", "7")
-      .replace("eight", "8")
-      .replace("nine", "9")
-  );
+function replaceStringWithNumber(str: string) {
+  return str
+    .replace("one", "1")
+    .replace("two", "2")
+    .replace("three", "3")
+    .replace("four", "4")
+    .replace("five", "5")
+    .replace("six", "6")
+    .replace("seven", "7")
+    .replace("eight", "8")
+    .replace("nine", "9");
+}
 
-  const numbers = replacedNumbers.match(numberRegex);
+for (const line of lines) {
+  var mock,
+    numbers = [];
+
+  while ((mock = wordRegex.exec(line))) {
+    numbers.push(mock[0]);
+    wordRegex.lastIndex = mock.index + 1; // avoid infinite loop
+  }
 
   if (numbers) {
-    const firstNumber = numbers[0];
-    const lastNumber = numbers[numbers.length - 1];
+    const firstNumber = replaceStringWithNumber(numbers[0]);
+    const lastNumber = replaceStringWithNumber(numbers[numbers.length - 1]);
 
     console.log(firstNumber, lastNumber);
 
